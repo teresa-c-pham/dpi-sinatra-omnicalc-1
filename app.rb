bcf01bb4fd9d687ec25d224ee467b83ec425664a
Teresa Pham <79329784+teresa-c-pham@users.noreply.github.com>
2024-07-10 23:38:26 +0000

    Implement square and square root

--- app.rb
+++ app.rb
@@ -2,8 +2,31 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:main)
+end
+
+get("/square/new") do
+  erb(:main)
+end
+
+get("/square_root/new") do
+  erb(:root)
+end
+
+get("/payment/new") do
+  erb(:payment)
+end
+
+get("/random/new") do
+  erb(:random)
+end
+
+get("/square/results") do
+  @input = params.fetch("number")
+  erb(:square_results)
+end
+
+get("/square_root/results") do
+  @input = params.fetch("number")
+  erb(:root_results)
 end
