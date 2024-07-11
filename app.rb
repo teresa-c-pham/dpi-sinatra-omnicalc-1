dc0b7c41bb96136b8b4ddc2740cab5b00e2375f9
Teresa Pham <79329784+teresa-c-pham@users.noreply.github.com>
2024-07-11 01:01:01 +0000

    Finalize results

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require "sinatra"
 require "sinatra/reloader"
+require "active_support/all"
 
 get("/") do
   erb(:main)
@@ -23,10 +24,27 @@ end
 
 get("/square/results") do
   @input = params.fetch("number")
+  @square = @input.to_f**2
   erb(:square_results)
 end
 
 get("/square_root/results") do
   @input = params.fetch("number")
+  @root = @input.to_f**0.5
   erb(:root_results)
+end
+
+get("/payment/results") do
+  @apr = params.fetch("apr_rate").to_f
+  @years = params.fetch("year").to_i
+  @principal = params.fetch("start").to_f
+  @payment = (@apr / 100 / 12) * @principal / (1 - (1 + (@apr / 100 / 12))**(-1 * 12 * @years))
+  erb(:payment_results)
+end
+
+get("/random/results") do
+  @min = params.fetch("min").to_f
+  @max = params.fetch("max").to_f
+  @random = rand(@min..@max)
+  erb(:random_results)
 end
